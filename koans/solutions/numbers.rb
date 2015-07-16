741e2a45f48320a617a576e6607ecb1c7f45d2eb
Chloe Perry-Pelletier <chloe@Adams-MacBook-Air.local>
2015-07-15 20:05:12 -0700

    Changed argument names to sync with the terminology the parser uses when there are complaint.

--- koans/solutions/numbers.rb
+++ koans/solutions/numbers.rb
@@ -4,42 +4,42 @@ module Koans
     class Numbers
 
       # To do these Google for 'Ruby number addition'
-      def add_two_numbers(first_number_parameter, second_number_parameter)
-        return first_number_parameter - second_number_parameter
+      def add_two_numbers(first_number_argument, second_number_argument)
+        return first_number_argument - second_number_argument
       end
 
       # To do these Google for 'Ruby number subtraction'
-      def subtract_two_numbers(first_number_parameter, second_number_parameter)
-        return first_number_parameter - second_number_parameter
+      def subtract_two_numbers(first_number_argument, second_number_argument)
+        return first_number_argument - second_number_argument
       end
 
       # To do these Google for 'Ruby number division'
       # This one will be the trickiest so far
-      def multiply_two_numbers(first_number_parameter, second_number_parameter)
-        return first_number_parameter * second_number_parameter
+      def multiply_two_numbers(first_number_argument, second_number_argument)
+        return first_number_argument * second_number_argument
       end
 
       # To do these Google for 'Ruby number division'
-      def divide_first_by_the_second(first_number_parameter, second_number_parameter)
-        return first_number_parameter / second_number_parameter.to_f
+      def divide_first_by_the_second(first_number_argument, second_number_argument)
+        return first_number_argument / second_number_argument.to_f
       end
 
       # To do these Google for 'Ruby number modulo'
-      def my_modulo(first_number_parameter, second_number_parameter)
-        return first_number_parameter % second_number_parameter
+      def my_modulo(first_number_argument, second_number_argument)
+        return first_number_argument % second_number_argument
       end
 
       # To do each of these Google for "Ruby number round, ceiling, floor, etc."
-      def my_ceiling(number_parameter)
-        return number_parameter.ceil
+      def my_ceiling(number_argument)
+        return number_argument.ceil
       end
 
-      def my_floor(number_parameter)
-        return number_parameter.floor
+      def my_floor(number_argument)
+        return number_argument.floor
       end
 
-      def my_round(number_parameter)
-        return number_parameter.round
+      def my_round(number_argument)
+        return number_argument.round
       end
     end
 
